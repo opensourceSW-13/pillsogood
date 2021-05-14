67e4565ad8555c4c668c8ffb71c4dd42a5a218fc
Yu Lim Kim <yulime0605@naver.com>
2021-05-14 23:29:28 +0900

    Initial commit

---
+++ app/src/main/java/com/opensource13/pillsogood/DaydruglistActivity.java
@@ -0,0 +1,14 @@
+package com.opensource13.pillsogood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DaydruglistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daydruglist);
+    }
+}
