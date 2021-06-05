7cb11f6cc251437c264be7a74699b0acd7ca8521
kyungyeoon <yeon981022@gmail.com>
2021-06-05 19:53:08 +0900

    디자인 수정전

--- app/src/main/java/com/opensource13/pillsogood/SetalarmActivity.java
+++ app/src/main/java/com/opensource13/pillsogood/SetalarmActivity.java
@@ -12,6 +12,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -20,14 +21,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.opensource13.pillsogood.SQLite.Mydrugitem;
 import com.opensource13.pillsogood.SQLite.SQLiteHelper;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 
 public class SetalarmActivity extends AppCompatActivity {
 
-    Calendar myCalendar = Calendar.getInstance();
-    SQLiteHelper helper = new SQLiteHelper(this);
-
+    TimePickerDialog picker;
 
     // Edit Text가 아닌 영역을 터치하는 경우, 키보드 내림
     @Override
@@ -53,6 +51,9 @@ public class SetalarmActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setalarm);
 
+        SQLiteHelper helper = new SQLiteHelper(this); // 헬퍼 선언
+
+
         // 복용 요일 체크박스
         final CheckBox cb_Mon = (CheckBox)findViewById(R.id.cb_Mon);
         final CheckBox cb_Tue = (CheckBox)findViewById(R.id.cb_Tue);
@@ -62,8 +63,41 @@ public class SetalarmActivity extends AppCompatActivity {
         final CheckBox cb_Sat = (CheckBox)findViewById(R.id.cb_Sat);
         final CheckBox cb_Sun = (CheckBox)findViewById(R.id.cb_Sun);
 
+        // 복용 시간 입력값 가져오기 (time picker 띄우기)
+        String time = "";
+        TextView pillTime = (TextView) findViewById(R.id.pillTime);
+        pillTime.setOnClickListener(new View.OnClickListener() {
 
-        // 이전 버튼 클릭 시 액티비티 전환
+            @Override
+            public void onClick(View v) {
+                Calendar cldr = Calendar.getInstance();
+                int hour = cldr.get(Calendar.HOUR_OF_DAY);
+                int minute = cldr.get(Calendar.MINUTE);
+
+                // Time picker Dialog
+                TimePickerDialog picker;
+
+                picker = new TimePickerDialog(SetalarmActivity.this, android.R.style.Theme_Holo_Dialog_NoActionBar ,new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        String state = "AM";
+                        // 선택한 시간이 12를 넘을경우 "PM"으로 변경 및 -12시간하여 출력 (ex : PM 6시 30분)
+                        //if (selectedHour > 12) {
+                        //    selectedHour -= 12;
+                        //    state = "PM";
+                        //}
+                        // EditText에 출력할 형식 지정
+                        pillTime.setText(selectedHour + ":" + selectedMinute);
+                        time.concat(selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, true); // true의 경우 24시간 형식의 TimePicker 출현
+                picker.getWindow().setBackgroundDrawableResource((android.R.color.transparent));
+                picker.show();
+            }
+        });
+
+
+        // 이전 버튼 클릭 시 화면3으로 액티비티 전환
         Button setAlarmPrevious = (Button) findViewById(R.id.setAlarmPrevious);
         setAlarmPrevious.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,7 +119,7 @@ public class SetalarmActivity extends AppCompatActivity {
 
                 // 약 이름 입력값 가져오기
                 EditText pillName = (EditText) findViewById(R.id.pillName);
-                String _name = pillName.getText().toString();;
+                String name = pillName.getText().toString();;
 
 
                 if (pillName.getText().toString().replace(" ", "").equals("")) { // 공백이면
@@ -95,63 +129,36 @@ public class SetalarmActivity extends AppCompatActivity {
 
 
                 // 복용 요일 입력값 가져오기
-                String _day = "";
-                if (cb_Mon.isChecked() == true) _day += cb_Mon.getText().toString() + " ";
-                if (cb_Tue.isChecked() == true) _day += cb_Tue.getText().toString() + " ";
-                if (cb_Wed.isChecked() == true) _day += cb_Wed.getText().toString() + " ";
-                if (cb_Thu.isChecked() == true) _day += cb_Thu.getText().toString() + " ";
-                if (cb_Fri.isChecked() == true) _day += cb_Fri.getText().toString() + " ";
-                if (cb_Sat.isChecked() == true) _day += cb_Sat.getText().toString() + " ";
-                if (cb_Sun.isChecked() == true) _day += cb_Sun.getText().toString() + " ";
+                String day = "";
+                if (cb_Mon.isChecked() == true) day += cb_Mon.getText().toString() + " ";
+                if (cb_Tue.isChecked() == true) day += cb_Tue.getText().toString() + " ";
+                if (cb_Wed.isChecked() == true) day += cb_Wed.getText().toString() + " ";
+                if (cb_Thu.isChecked() == true) day += cb_Thu.getText().toString() + " ";
+                if (cb_Fri.isChecked() == true) day += cb_Fri.getText().toString() + " ";
+                if (cb_Sat.isChecked() == true) day += cb_Sat.getText().toString() + " ";
+                if (cb_Sun.isChecked() == true) day += cb_Sun.getText().toString() + " ";
 
 
-
-                // 복용 시간 입력값 가져오기
-                String _time = "";
-                EditText pillTime = (EditText) findViewById(R.id.pillTime);
-                pillTime.setOnClickListener(new View.OnClickListener() {
-
-                    @Override
-                    public void onClick(View v) {
-                        Calendar mcurrentTime = Calendar.getInstance();
-                        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-                        int minute = mcurrentTime.get(Calendar.MINUTE);
-                        TimePickerDialog mTimePicker;
-                        mTimePicker = new TimePickerDialog(SetalarmActivity.this, new TimePickerDialog.OnTimeSetListener() {
-                            @Override
-                            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                                String state = "AM";
-                                // 선택한 시간이 12를 넘을경우 "PM"으로 변경 및 -12시간하여 출력 (ex : PM 6시 30분)
-                                if (selectedHour > 12) {
-                                    selectedHour -= 12;
-                                    state = "PM";
-                                }
-                                // EditText에 출력할 형식 지정
-                                pillTime.setText(state + " " + selectedHour + ":" + selectedMinute);
-                                _time += state + " " + selectedHour + ":" + selectedMinute;
-                            }
-                        }, hour, minute, false); // true의 경우 24시간 형식의 TimePicker 출현
-                        mTimePicker.setTitle("Select Time");
-                        mTimePicker.show();
-                    }
-                });
+                // 메모 입력값 가져오기
+                EditText pillMemo = (EditText)findViewById(R.id.pillMemo);
+                String memo = pillMemo.getText().toString();
 
 
-                //DB 목록 추가
-                public void Insertdrug (String _name, String _day, String _time) {
-                    SQLiteDatabase db = helper.getWritableDatabase();
-                    db.execSQL("INSERT INTO MYDRUG (name, day, time) VALUES('" + _name + "', '" + _day + "', '" + _time + "');");
-                }
+                // DB에 항목 추가
+                SQLiteDatabase db;
+
+                db = helper.getWritableDatabase();
+                db.execSQL("insert into MYDRUG values('"+name+"', '"+day+"', '"+time+"', '"+memo+"')");
 
 
-                // 액티비티 전환
+                // 화면 3으로 액티비티 전환
                 Intent intent = new Intent(getApplicationContext(), MydrugActivity.class);
                 startActivity(intent);
 
             }
 
-        });
 
+        });
 
     }
 }
